f2aa73e41917433938b3cd32c792431753c1360f
Victor <victorformisano@gmail.com>
2023-07-29 00:11:46 -0300

    chore: folder and files update

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,46 +1,7 @@
-import React from 'react'
 
 const Settings = () => {
   return (
-    <div>
-       <div>
-            <h1>Meu perfil</h1>
-            <img></img>
-            <h3>Bisteca violeiro</h3>
-            <h3>Consultor Financeiro</h3>
-            <h4>Ribeirão Preto-SP</h4>
-        </div>
-
-        <div>
-            <button>Edit</button>
-            <img src="https://th.bing.com/th/id/R.135733756def716f8c7929825dea2892?rik=KuQOmYasOfZDxQ&pid=ImgRaw&r=0"/>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
-  
-
-    
-
-
-   
+    <div>Settings</div>
   )
 }
 
